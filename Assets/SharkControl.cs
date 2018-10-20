ec9ac12316170323dcc7b14be836676925ea8821
Dalox <jdnl95@gmail.com>
2018-10-20 16:27:49 -0500

    DO NOT MERGE: Control del minitiburon, con navmesh que no funciona

---
+++ Assets/SharkControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkControl : MonoBehaviour {
+
+	public Transform[] points;
+	private int destPoint;
+	private UnityEngine.AI.NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		agent.autoBraking = false;
+		GotoNextPoint();
+	}
+
+	void GotoNextPoint(){
+		if (points.Length == 0)
+		{
+			return;
+		}
+
+		agent.destination = points[destPoint].position;
+
+		destPoint = (destPoint + 1) % points.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!agent.pathPending && agent.remainingDistance < 0.5f){
+			GotoNextPoint();
+		}
+	}
+}
